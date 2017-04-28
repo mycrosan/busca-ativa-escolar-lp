17079db42f431b6fcd00479040896f646ae03914
Aryel Tupinambá <aryel.tupinamba@lqdi.net>
2017-04-28 15:48:44 -0300

    Added exception display

--- public/enviar.php
+++ public/enviar.php
@@ -1,10 +1,14 @@
 <?php
-
 header('Content-Type: application/json');
-if (empty($_POST)) {die(json_encode(["status" => false, "mensagem" => "requisição inválida."]));}
+
+if (empty($_POST)) {
+	die(json_encode(["status" => false, "mensagem" => "Por favor, preencha todos os campos."]));
+}
 
 require '../vendor/autoload.php';
+
 use Mailgun\Mailgun;
+
 $dotenv = new Dotenv\Dotenv(__DIR__."/..");
 $dotenv->load();
 
@@ -17,6 +21,7 @@ $post = [
 ];
 
 $body = "Segue dados do contato: <br /><br />";
+
 foreach($post as $label => $value) {
     $body .= "<b>$label</b>: <i>".($value ? $value : "N/D")."</i><br />";
 }
@@ -26,15 +31,19 @@ $key = getenv("MAILGUN_KEY");
 $domain = getenv("MAILGUN_DOMAIN");
 $from = getenv("CONTACT_FROM");
 $to = getenv("CONTACT_TO");
+
 $mgClient = new Mailgun($key);
+
 try {
-$result = $mgClient->sendMessage("$domain",
-    array('from'    => $from,
+	$result = $mgClient->sendMessage("$domain", [
+		'from'    => $from,
         'to'      => $to,
         'subject' => 'Novo envio de contato.',
-        'html'    => $body));
+        'html'    => $body
+	]);
+
 } catch (Exception $e) {
-    die(json_encode(["status" => false, "mensagem" => "Ops! Estamos sem sistema no momento."]));
+    die(json_encode(["status" => false, "mensagem" => "Ops! Estamos sem sistema no momento.", "exception" => $e]));
 }
 
 die(json_encode(["status" => true, "mensagem" => "Seu contato foi para análise, entraremos em contato assim que possível."]));
