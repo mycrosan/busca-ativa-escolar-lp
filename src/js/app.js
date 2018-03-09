ca5dbab1e67824a9d6be9fe4220079a6aa0e3ebc
Luiz Fernando de Lira <luiz.lira@lqdi.net>
2018-03-09 16:43:05 -0300

    adjusted preferential phone visibility

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 //=require libs/_10_masks.js
 //=require libs/_sweetalert.js
+//=require data/estados_municipios.js
 
 // Scroll
 $(function() {
@@ -53,3 +54,29 @@ $('form').submit(function(e) {
 			: swal("Ops...", data.mensagem, "error")
 	});
 });
+
+//SELECT ESTADOS E MUNICÍPIOS
+$(document).ready(function(){
+	$estados = $('#estados');
+	$municipios = $('#cidades');
+	unidades_fededativas.forEach(function(uf){
+		var uf_option = document.createElement("option");
+		uf_option.value = uf.sigla;
+		uf_option.text = uf.sigla;
+		$estados.append(uf_option);
+	});
+});
+
+$("#estados").on("change", function(){
+	$("#cidades").children().not(':first').remove();
+	var estado = $('#estados').val();
+    unidades_fededativas.forEach(function(uf){
+		if(estado != uf.sigla){return}
+        uf.cidades.forEach(function(municipio){
+            var municipio_option = document.createElement("option");
+            municipio_option.value = municipio;
+            municipio_option.text = municipio;
+            $municipios.append(municipio_option);
+        });
+	});
+})
